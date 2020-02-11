1d3a34403aafd2d03ff50ce5e5977a51ea000222
Alex Martinez <alexmartinez0071234@gmail.com>
2020-02-11 16:01:11 -0500

    stopped the links having to refresh the page and changed button text when clicked on

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@
 
 const siteImgs = document.querySelectorAll('img');
 
+// when hovered over imgs increase sale and return to normal when off
 siteImgs.forEach(img=>{
     img.addEventListener('mouseover',()=>{
         img.style.transform='scale(1.1)';
@@ -13,6 +14,7 @@ siteImgs.forEach(img=>{
     });
 });
 
+// on click imgs rotate 180 degrees
 siteImgs.forEach(img=>{
     img.addEventListener('click',()=>{
         img.style.transform='rotate(180deg)'
@@ -21,7 +23,25 @@ siteImgs.forEach(img=>{
 
 const siteTitle = document.querySelector('.logo-heading')
 
+//  when title is clicked it changes to hi mom and color goes to purple
 siteTitle.addEventListener('dblclick',()=>{
     siteTitle.textContent='HI MOM';
     siteTitle.style.color='purple';
+});
+
+const stopLinks=document.querySelectorAll('.nav-link')
+
+// stops links from refreshing the page
+stopLinks.forEach(link=>{
+    link.addEventListener('click',(event)=>{
+        event.preventDefault();
+    });
+});
+
+const buttons = document.querySelectorAll('.btn')
+
+buttons.forEach(button=>{
+    button.addEventListener('click',()=>{
+        button.textContent='This Button actually does nothing'
+    })
 })
