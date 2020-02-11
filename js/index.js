64bd9cecc625f6aa6cd479eb9889c4df201af6b6
Alex Martinez <alexmartinez0071234@gmail.com>
2020-02-11 15:19:16 -0500

    made it so when hovered over images scale 1.1 and return to 1 when not

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+
+
+const siteImgs = document.querySelectorAll('img');
+
+siteImgs.forEach(img=>{
+    img.addEventListener('mouseenter',()=>{
+        img.style.transform='scale(1.1)';
+    });
+
+    img.addEventListener('mouseleave',()=>{
+        img.style.transform='scale(1)';
+    });
+});
