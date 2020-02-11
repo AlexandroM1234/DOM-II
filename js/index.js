ad925630ba175eeeff26190d3ce6d0a9159306e8
Alex Martinez <alexmartinez0071234@gmail.com>
2020-02-11 15:41:27 -0500

    made imgs rotate 180 when clicked and when Fun Bus is click it says HI MOM and changes color to purple

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@
 const siteImgs = document.querySelectorAll('img');
 
 siteImgs.forEach(img=>{
-    img.addEventListener('mouseenter',()=>{
+    img.addEventListener('mouseover',()=>{
         img.style.transform='scale(1.1)';
     });
 
@@ -12,3 +12,16 @@ siteImgs.forEach(img=>{
         img.style.transform='scale(1)';
     });
 });
+
+siteImgs.forEach(img=>{
+    img.addEventListener('click',()=>{
+        img.style.transform='rotate(180deg)'
+    });
+});
+
+const siteTitle = document.querySelector('.logo-heading')
+
+siteTitle.addEventListener('dblclick',()=>{
+    siteTitle.textContent='HI MOM';
+    siteTitle.style.color='purple';
+})
